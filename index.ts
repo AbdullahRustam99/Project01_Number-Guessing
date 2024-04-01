69c56874cbfb433e0976ecea6e979f7a2b53069e
unknown <abdullahrustam12345@gmail.com>
2024-04-01 12:22:47 +0500

    Push Changes

--- index.ts
+++ index.ts
@@ -3,20 +3,62 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-
-const number:number = Math.floor(Math.random() *10);
-
+let trys = 3
+let condition = true
 console.log(chalk.bold.hex("#FA0093")("WELCOME TO NUMBER GUESSING GAME"));
-const guess = await inquirer.prompt([{
-	types : "number",
-	name : "guessNumber",
-	message : "Please Guess The Number Between 1-10 :"
-}]);
 
 
-if(guess.guessNumber == number){
-	console.log(chalk.bold.italic.hex("#09F000")("Congratulations you guess the Correct Number"))
+while (trys >= 0) {
+	const number: number = Math.floor(Math.random() * 9 + 1);
 
-}else{
-	console.log(chalk.bold.italic.hex("#FF0B01")("Sorry you guess the Wrong Number"));
+	const guess = await inquirer.prompt([{
+		types: "number",
+		name: "guessNumber",
+		message: "Please Guess The Number Between 1-10 :"
+	}]);
+
+	if (guess.guessNumber == number) {
+		console.log(chalk.bold.italic.hex("#09F000")("Congratulations you guess the Correct Number"));
+		console.log(chalk.bold.italic.hex("#D3D600")(`You have ${trys} try left!!!`));
+	} else {
+		console.log(chalk.bold.italic.hex("#FF0B01")("Sorry you guess the Wrong Number"));
+		console.log(chalk.bold.italic.hex("#D3D600")(`You have ${trys} try left!!!`));
+
+	}
+	trys -= 1
+}
+while (condition) {
+	let tryAgain = await inquirer.prompt([{
+		type: "confirm",
+		name: "try",
+		message: "Do you want to play again",
+		default: "false"
+	}])
+	condition = tryAgain.try
+	if (condition == true) {
+		let trys = 4
+		while (trys >= 0) {
+			const number: number = Math.floor(Math.random() * 9 + 1);
+
+			const guess = await inquirer.prompt([{
+				types: "number",
+				name: "guessNumber",
+				message: "Please Guess The Number Between 1-10 :"
+			}]);
+
+			if (guess.guessNumber == number) {
+				console.log(chalk.bold.italic.hex("#09F000")("Congratulations you guess the Correct Number"));
+				console.log(chalk.bold.italic.hex("#D3D600")(`You have ${trys} try left!!!`));
+
+			} else {
+				console.log(chalk.bold.italic.hex("#FF0B01")("Sorry you guess the Wrong Number"));
+				console.log(chalk.bold.italic.hex("#D3D600")(`You have ${trys} try left!!!`));
+			}
+			trys -= 1
+		}
+	} else {
+		console.log(chalk.bold.hex("#E87800")("GoodBy Come Back Later"));
+
+	}
+
 }
